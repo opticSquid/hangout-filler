4db21cdaf0e10dcb2b0fda81905cc07b3818857d
Soumalya Bhattacharya <soumalyabhattacharya6@gmail.com>
2025-04-13 16:05:47 +0530

    made the go routines finish normally when work finishes but also kept a way to stop all threads when os interruprt ococurs

--- main.go
+++ main.go
@@ -2,7 +2,12 @@ package main
 
 import (
 	"context"
-	"time"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+
+	"github.com/rs/zerolog/log"
 
 	"github.com/knadh/koanf/v2"
 	"hangoutsb.in/filler-service/appconfigmanager"
@@ -17,10 +22,25 @@ func main() {
 	loggerconfig.InitLogger()
 	appconfigmanager.InitAppConfig(CONFIG)
 	loggerconfig.SetGlobalLogLevel(CONFIG)
-	userPool := CONFIG.Int("app.user-pool")
-	context, cancel := context.WithTimeout(context.Background(), 1*time.Minute)
+
+	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
+
+	osInterruptChan := make(chan os.Signal, 1)
+	signal.Notify(osInterruptChan, syscall.SIGINT, syscall.SIGTERM)
+
+	var userPoolWaitGroup sync.WaitGroup
+	userPool := CONFIG.Int("app.user-pool")
 	for i := range userPool {
-		go user.CreateUser(i, context)
+		userPoolWaitGroup.Add(1)
+		go user.CreateUser(i, ctx, &userPoolWaitGroup)
 	}
+
+	go func() {
+		<-osInterruptChan
+		log.Info().Msg("os interrupt recieved, sending signal to workers to stop immidietly...")
+		cancel()
+	}()
+	userPoolWaitGroup.Wait()
+	log.Info().Msg("all workers finished, exiting program normally")
 }
--- user/user.go
+++ user/user.go
@@ -2,18 +2,21 @@ package user
 
 import (
 	"context"
+	"sync"
 
 	"github.com/rs/zerolog/log"
 )
 
-func CreateUser(userNumber int, ctx context.Context) {
+func CreateUser(userNumber int, ctx context.Context, userWaitGroup *sync.WaitGroup) {
 
 	log.Debug().Str("woker-type", "user").Int("worker-number", userNumber).Msg("worker started")
-	defer log.Debug().Str("woker-type", "user").Int("worker-number", userNumber).Msg("worker stopped")
+	defer userWaitGroup.Done()
+	defer log.Debug().Str("worker-type", "user").Int("worker-number", userNumber).Msg("Worker finished and exiting.")
 	select {
 	case <-ctx.Done():
-		log.Debug().Str("woker-type", "user").Int("worker-number", userNumber).Msg("worker finished job")
+		log.Info().Str("woker-type", "user").Int("worker-number", userNumber).Msg("stopping due to context cancellation")
 	default:
 		log.Debug().Str("woker-type", "user").Int("worker-number", userNumber).Msg("worker doing its job")
 	}
+	log.Info().Str("worker-type", "user").Int("worker-number", userNumber).Msg("Worker completed all its tasks naturally.")
 }
